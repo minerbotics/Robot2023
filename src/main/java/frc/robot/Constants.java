77cbab406ec792811d286b31c62541940a933776
Cory Richards <crichards@negauneeschools.onmicrosoft.com>
2023-01-28 11:56:58 -0500

    Added subsystem classes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -28,23 +28,23 @@ public final class Constants {
 
     public static final int DRIVETRAIN_PIGEON_ID = 0; // FIXME Set Pigeon ID
 
-    public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 8; 
-    public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 7; 
+    public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 7; 
+    public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 8; 
     public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 11;
-    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(325.7226510266903);
+    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0);
 
     public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 1; 
     public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 2; 
     public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 12; 
-    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(179.82422326047688); 
+    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0); 
 
     public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 5; 
     public static final int BACK_LEFT_MODULE_STEER_MOTOR = 6; 
     public static final int BACK_LEFT_MODULE_STEER_ENCODER = 9;
-    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(350.06836854958055);
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0);
 
     public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 3;
     public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 4;
     public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 10;
-    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(270.0878915574851);
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0);
 }

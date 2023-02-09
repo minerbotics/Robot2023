eeaf30ff81551ffc0e56e9cf555a32ed6385b8b3
Minerbotics <frcteam4827@gmail.com>
2023-02-08 20:03:55 -0500

    add command to center robot on target

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -6,6 +6,7 @@ import java.time.ZonedDateTime;
 
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Limelight extends SubsystemBase {

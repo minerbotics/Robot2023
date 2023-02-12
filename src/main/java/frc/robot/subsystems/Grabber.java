04852e752a97a942cf386d151c88c8e54aac5c31
Michael Kinnunen <michael.kinnunen@civicactions.com>
2023-02-12 16:54:59 -0500

    Finish pneumatics subsystems, start Arm

---
+++ src/main/java/frc/robot/subsystems/Grabber.java
@@ -0,0 +1,32 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.GrabberConstants;
+
+public class Grabber extends SubsystemBase {
+
+    private final DoubleSolenoid m_grabberSolenoid;
+
+    public Grabber() {
+        m_grabberSolenoid = new DoubleSolenoid(
+            PneumaticsModuleType.CTREPCM, 
+            GrabberConstants.FORWARD_CHANNEL, 
+            GrabberConstants.REVERSE_CHANNEL
+        );
+    }
+
+    public void grab() {
+        m_grabberSolenoid.set(Value.kForward);
+    }
+
+    public void release() {
+        m_grabberSolenoid.set(Value.kReverse);
+    }
+
+    public void toggleGrabber() {
+        m_grabberSolenoid.toggle();
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/Slider.java
@@ -0,0 +1,36 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.SliderConstants;
+
+public class Slider extends SubsystemBase {
+
+    private final DoubleSolenoid m_sliderSolenoid;
+
+    public Slider() {
+        m_sliderSolenoid = new DoubleSolenoid(
+            PneumaticsModuleType.CTREPCM, 
+            SliderConstants.FORWARD_CHANNEL, 
+            SliderConstants.REVERSE_CHANNEL
+        );
+    }
+
+    public void slideIn() {
+        m_sliderSolenoid.set(Value.kReverse);
+    }
+
+    public void slideOut() {
+        m_sliderSolenoid.set(Value.kForward);
+    }
+
+    public void toggleSlider() {
+        m_sliderSolenoid.toggle();
+    }
+
+    public boolean isOut() {
+        return m_sliderSolenoid.get() == Value.kForward;
+    }
+}

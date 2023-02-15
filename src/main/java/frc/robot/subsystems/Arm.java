29e039b9d29e9a9745236a83230148fae36e3fbd
Michael Kinnunen <michael.kinnunen@civicactions.com>
2023-02-15 11:44:27 -0500

    Add Arm subsystem

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,10 +1,56 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax.ControlType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ArmConstants;
 
 public class Arm extends SubsystemBase {
 
+    private final CANSparkMax m_rightArmMotor;
+    private final CANSparkMax m_leftArmMotor;
+    private final SparkMaxPIDController m_pidController;
+
+    public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput;
+
     public Arm() {
+        m_rightArmMotor = new CANSparkMax(
+            ArmConstants.RIGHT_ARM_MOTOR, 
+            CANSparkMaxLowLevel.MotorType.kBrushless);
+        m_leftArmMotor = new CANSparkMax(
+            ArmConstants.LEFT_ARM_MOTOR, 
+            CANSparkMaxLowLevel.MotorType.kBrushless);
+        m_leftArmMotor.follow(m_rightArmMotor, true);
+        
+        // Get the PID controller for the pivot motor
+        m_pidController = m_rightArmMotor.getPIDController();
+
+        // set PID coefficients
+        m_pidController.setP(ArmConstants.PID_P);
+        m_pidController.setI(ArmConstants.PID_I);
+        m_pidController.setD(ArmConstants.PID_D);
+        m_pidController.setIZone(ArmConstants.PID_Iz);
+        m_pidController.setFF(ArmConstants.PID_FF);
+        m_pidController.setOutputRange(
+            ArmConstants.PID_MIN_OUTPUT_RANGE, 
+            ArmConstants.PID_MAX_OUTPUT_RANGE);
     }
-    
+
+    public void setPivotSetpoint(double setpoint) {
+        m_pidController.setReference(setpoint, ControlType.kPosition);
+    }
+
+    public void setPivotSpeed(double speed) {
+        m_rightArmMotor.set(speed);
+    }
+
+    public void resetPivot() {
+        m_pidController.setIAccum(0.0);
+    }
+
+    public void setStop() {
+        m_rightArmMotor.set(0);
+    }
 }

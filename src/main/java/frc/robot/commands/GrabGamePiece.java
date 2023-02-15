040c1a6717dd1aec3f827f6702203146b7c50ee7
Michael Kinnunen <michael.kinnunen@civicactions.com>
2023-02-15 11:43:12 -0500

    Remove slider dependency from GrabGamePiece

--- src/main/java/frc/robot/commands/GrabGamePiece.java
+++ src/main/java/frc/robot/commands/GrabGamePiece.java
@@ -2,24 +2,18 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Grabber;
-import frc.robot.subsystems.Slider;
 
 public class GrabGamePiece extends CommandBase {
 
     private final Grabber m_grabber;
-    private final Slider m_slider;
 
-    public GrabGamePiece(Grabber grabber, Slider slider) {
+    public GrabGamePiece(Grabber grabber) {
         m_grabber = grabber;
-        m_slider = slider;
-        addRequirements(m_grabber, m_slider);
+        addRequirements(m_grabber);
     }
 
     @Override
     public void initialize() {
-        if(!m_slider.isOut()) {
-            return;
-        }
         m_grabber.grab();
     }
 

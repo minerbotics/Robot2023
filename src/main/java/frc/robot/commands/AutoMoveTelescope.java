e71a851ba34a060b51bcac8f7c90c50d28fb4856
FRC Team 4827 - Minerbotics <60262671+minerbotics@users.noreply.github.com>
2023-03-24 10:22:42 -0400

    New Branch

--- src/main/java/frc/robot/commands/AutoMoveTelescope.java
+++ src/main/java/frc/robot/commands/AutoMoveTelescope.java
@@ -1,7 +1,6 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.ArmTelescope;
 
 public class AutoMoveTelescope extends CommandBase {
